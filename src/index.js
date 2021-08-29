09b2ce814066835fbbbf44a0db91ec7711589983
Christopher McGale <mcga6260@mylaurier.ca>
2021-08-29 13:50:46 -0400

    Section 12 done

--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json())
+// app.use((req, res, next) => {
+//     res.status(503).send("Site is currently down, check back soon!")
+// })
 
+// app.use((req, res, next) => {
+//     if (req.method == "GET") {
+//         res.send("Get requests are disabled")
+//     } else {
+//         next()
+//     }
+// })
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,44 +1,123 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Task = require('./task')
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        age: {
+            type: Number,
+            default: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number')
+                }
             }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-                throw new Error('Email is invalid!')
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            validate(value) {
+                if(!validator.isEmail(value)) {
+                    throw new Error('Email is invalid!')
+                }
             }
-        }
-        
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error('Password cannot include "password"')
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 7,
+            trim: true,
+            validate(value) {
+                if (value.toLowerCase().includes("password")) {
+                    throw new Error('Password cannot include "password"')
+                }
             }
-        }
+        },
+        tokens: [{
+            token:
+            { 
+                type: String,
+                required: true
+            }
+        }]
     }
+)
+
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
 })
+
+userSchema.methods.genToken = async function () {
+    const user = this
+    // Optional third arg for expiry ex. {'7 days'}
+    const token = jwt.sign({ _id: user._id.toString() }, 'secretKey')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+userSchema.methods.toJSON = async function () {
+    const user = this
+    const userObject = user.toObject()
+
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    
+    if (!user) {
+        throw new Error('Invalid email or password')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Invalid email or password')
+    }
+
+    return user
+}
+
+
+// Hash plane text password before saving
+// Must be normal function to use this
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+//Delete user tasks when user is removed
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id })
+    next()
+})
+
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
